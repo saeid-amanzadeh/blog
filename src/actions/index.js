be30ac98347bbe0871c20f184ad37b24aeffd0ae
Saeid Amanzadeh <saeid.amanzadehgharehtappeh@pearson.com>
2020-01-17 00:09:28 -0600

    finished the initial implemetation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,18 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+    //a cool syntax of the above commented lines
+    _.chain(getState().posts)
+            .map('userId')
+            .uniq()
+            .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
@@ -7,7 +21,6 @@ export const fetchPosts = () => async dispatch => {
         payload: response.data
     });
 };
-
 export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({
@@ -15,3 +28,4 @@ export const fetchUser = id => async dispatch => {
         payload: response.data
     });
 };
+
